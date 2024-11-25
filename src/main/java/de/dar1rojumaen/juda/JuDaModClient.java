fbe6d3aac7e8fa5875d28a4b7bcc296ae658a565
JuMaEn <jumaen401@gmail.com>
2024-11-25 14:56:29 +0100

    Many Data Generations

---
+++ src/main/java/de/dar1rojumaen/juda/JuDaModClient.java
@@ -0,0 +1,9 @@
+package de.dar1rojumaen.juda;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JuDaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
